5e80828750d3a798658726c6bb0f230ae933a136
Walter0210 <walter.ebalderrama@gmail.com>
2020-11-11 17:32:58 -0300

    pedido

---
+++ CU121/Dominio/Pedido.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CU121.Dominio
+{
+    class Pedido
+    {
+        private int cantComensales;
+        private DateTime fechaHoraPedido;
+        private int nroPedido;
+
+        public Pedido()
+        {
+
+        }
+
+        public Pedido(int cantComensales, DateTime fechaHoraPedido, int nroPedido)
+        {
+            this.cantComensales = cantComensales;
+            this.fechaHoraPedido = fechaHoraPedido;
+            this.nroPedido = nroPedido;
+        }
+
+        public int CantComensales
+        {
+            get => cantComensales;
+            set => cantComensales = value;
+        }
+        public DateTime FechaHoraPedido
+        {
+            get => fechaHoraPedido;
+            set => fechaHoraPedido = value;
+        }
+        public int NroPedido
+        {
+            get => nroPedido;
+            set => nroPedido = value;
+        }
+
+
+    }
+}
